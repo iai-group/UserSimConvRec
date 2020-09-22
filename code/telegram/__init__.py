75e51e8fc189a74d13141375475eb172cbd8c1b8
Shuo Zhang <imsure318@163.com>
2020-09-22 22:12:47 +0100

    Token removal

--- code/telegram/__init__.py
+++ code/telegram/__init__.py
@@ -2,6 +2,5 @@
 We use telegram to collect human-machine dialogues.
 """
 
-TOKEN = "994935534:AAG8hjL-0duH6xUzd6rq96JZfkHI0ZAZE-g"
-# TOKEN = "1059989190:AAETb1p5olsQVy2Cc4p98pAHGjvDh5BKigY"
+TOKEN = "TODO: create your own"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
